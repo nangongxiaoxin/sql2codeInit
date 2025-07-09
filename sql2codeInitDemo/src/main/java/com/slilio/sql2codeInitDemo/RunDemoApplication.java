cb5d03c5e20611e724485bd13088a2b654aae0cc
nangongxiaoxin <bx_tianhuan@163.com>
2025-07-09 20:22:04 +0800

    init project

---
+++ sql2codeInitDemo/src/main/java/com/slilio/sql2codeInitDemo/RunDemoApplication.java
@@ -0,0 +1,14 @@
+package com.slilio.sql2codeInitDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: slilio @CreateTime: 2025-07-09 @Description: @Version: 1.0
+ */
+@SpringBootApplication
+public class RunDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RunDemoApplication.class, args);
+    }
+}
