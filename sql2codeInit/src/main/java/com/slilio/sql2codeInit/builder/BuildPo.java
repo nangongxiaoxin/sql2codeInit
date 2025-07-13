b1577aef85c629f3bae8e0d03498a80becd07fb7
nangongxiaoxin <bx_tianhuan@163.com>
2025-07-13 21:45:01 +0800

    PO文件写入

--- sql2codeInit/src/main/java/com/slilio/sql2codeInit/builder/BuildPo.java
+++ sql2codeInit/src/main/java/com/slilio/sql2codeInit/builder/BuildPo.java
@@ -2,24 +2,73 @@ package com.slilio.sql2codeInit.builder;
 
 import com.slilio.sql2codeInit.bean.Constants;
 import com.slilio.sql2codeInit.bean.TableInfo;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @Author: slilio @CreateTime: 2025-07-13 @Description: 持久化对象 @Version: 1.0
  */
 public class BuildPo {
+  private static final Logger logger = LoggerFactory.getLogger(BuildPo.class);
+
   public static void execute(TableInfo tableInfo) {
     File folder = new File(Constants.PATH_PO);
     if (!folder.exists()) {
       folder.mkdirs();
     }
 
-    File file = new File(folder, tableInfo.getBeanName() + ".java");
+    File poFile = new File(folder, tableInfo.getBeanName() + ".java");
+
+    OutputStream out = null;
+    OutputStreamWriter outw = null;
+    BufferedWriter bw = null;
+
     try {
-      file.createNewFile();
-    } catch (IOException e) {
-      e.printStackTrace();
+      out = new FileOutputStream(poFile);
+      outw = new OutputStreamWriter(out, StandardCharsets.UTF_8);
+      bw = new BufferedWriter(outw);
+
+      // 文件内容正文
+
+      bw.write("package " + Constants.PACKAGE_PO + ";");
+      bw.newLine();
+      bw.newLine();
+
+      bw.write("import java.io.Serializable;");
+      bw.newLine();
+      bw.newLine();
+
+      bw.write("public class " + tableInfo.getBeanName() + " implements Serializable {");
+      bw.newLine();
+      bw.write("}");
+
+      bw.flush();
+    } catch (Exception e) {
+      logger.error("创建PO失败", e);
+    } finally {
+      if (bw != null) {
+        try {
+          bw.close();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+      if (outw != null) {
+        try {
+          outw.close();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+      if (out != null) {
+        try {
+          out.close();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
     }
   }
 }
---
+++ sql2codeInitDemo/src/main/java/com/slilio/sql2codeInitDemo/entity/po/ProductInfo.java
@@ -0,0 +1,6 @@
+package com.slilio.sql2codeInitDemo.entity.po;
+
+import java.io.Serializable;
+
+public class ProductInfo implements Serializable {
+}
